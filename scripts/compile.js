9d4477d4878a57d33bf168ef6616c1f7dba94681
Harrison Beckerich <hbeckeri@purdue.edu>
2018-06-06 00:11:22 -0400

    master update compile script to output builds

--- scripts/compile.js
+++ scripts/compile.js
@@ -8,9 +8,21 @@ function findImports(path) {
 }
 
 const vanimalCore = fs.readFileSync(`contracts/KittyCore.sol`).toString();
-const input = { 'VanimalCore.sol': vanimalCore };
+const input = { 'KittyCore.sol': vanimalCore };
 
 const output = solc.compile({ sources: input }, 0, findImports);
 
-console.log(output);
+console.log(JSON.stringify(output.errors, null, 2));
 
+Object.keys(output.contracts).forEach(name => {
+	const abi = output.contracts[name].interface;
+	const bytecode = output.contracts[name].bytecode;
+
+	name = name.split(':')[1];
+
+	fs.writeFileSync(`contracts/build/${name}.abi`, abi);
+	console.log(`wrote contracts/build/${name}.abi`);
+
+	fs.writeFileSync(`contracts/build/${name}.bytecode`, bytecode);
+	console.log(`wrote contracts/build/${name}.bytecode`);
+});
